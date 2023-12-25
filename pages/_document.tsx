5ac132b27c488bc093f34ef2bdf9be1d98f9fd39
Hieu Vu <vdhieu.it@gmail.com>
2023-12-25 16:40:17 +0700

    chore: add plausible

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,11 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+        <script
+          defer
+          data-domain="sticker.console.so"
+          src="https://plausible.io/js/script.js"
+        ></script>
       </Head>
       <body>
         <Main />
